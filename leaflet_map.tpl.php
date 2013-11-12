7a91292f2234d63170c9e59f98aa4f5d8d016160
tomasbreffitt <tomasbreffitt@1988762.no-reply.drupal.org>
2013-11-11 16:59:18 -0800

    Fix Zoom control showing over top of admin overlay.

--- leaflet_map.tpl.php
+++ leaflet_map.tpl.php
@@ -4,4 +4,4 @@
  * Template for a leaflet map
  */
 ?>
-<div id="<?php print $map_id ?>" style="height: <?php print $height ?>"></div>
+<div id="<?php print $map_id ?>" style="height: <?php print $height ?>; z-index: 499;"></div>
