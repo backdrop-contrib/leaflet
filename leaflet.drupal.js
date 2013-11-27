c054831787d028cb2a04a65d147a5f4573972238
uniquename <uniquename@532810.no-reply.drupal.org>
2013-11-27 13:38:21 -0800

    #2121767 by uniquename: Trigger leaflet.feature for features in a group.

--- leaflet.drupal.js
+++ leaflet.drupal.js
@@ -51,6 +51,9 @@
                 lFeature.bindPopup(groupFeature.popup);
               }
               lGroup.addLayer(lFeature);
+
+              // Allow others to do something with the feature within a group.
+              $(document).trigger('leaflet.feature', [lFeature, feature]);
             }
 
             // add the group to the layer switcher
@@ -65,10 +68,11 @@
             if (feature.popup) {
               lFeature.bindPopup(feature.popup);
             }
+
+            // Allow others to do something with the feature.
+            $(document).trigger('leaflet.feature', [lFeature, feature]);
           }
 
-          // Allow others to do something with the feature that was just added to the map
-          $(document).trigger('leaflet.feature', [lFeature, feature]);
         }
 
         // add layer switcher
