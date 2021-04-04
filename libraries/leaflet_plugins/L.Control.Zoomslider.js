32563acde26ff73036b7e07657ce068e8ca2b339
indigoxela <user@olga.indigofloat.at>
2021-04-04 12:07:13 +0200

    Updated plugins

--- libraries/leaflet_plugins/L.Control.Zoomslider.js
+++ libraries/leaflet_plugins/L.Control.Zoomslider.js
@@ -77,7 +77,7 @@
 			options: {
 				position: 'topleft',
 				// Height of zoom-slider.png in px
-				stepHeight: 4, // jfd 8,
+				stepHeight: 8,
 				// Height of the knob div in px (including border)
 				knobHeight: 6,
 				styleNS: 'leaflet-control-zoomslider'
