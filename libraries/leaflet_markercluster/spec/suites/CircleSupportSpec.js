ad2d17230cda8bf10f5f7a02842e2de1c3d58c62
indigoxela <user@olga.indigofloat.at>
2021-04-04 11:20:44 +0200

    Get the main libs to their current stable version

--- libraries/leaflet_markercluster/spec/suites/CircleSupportSpec.js
+++ libraries/leaflet_markercluster/spec/suites/CircleSupportSpec.js
@@ -1,5 +1,9 @@
 ﻿describe('support for Circle elements', function () {
-	var map, div, clock;
+	/////////////////////////////
+	// SETUP FOR EACH TEST
+	/////////////////////////////
+	var clock, div, map, group;
+
 	beforeEach(function () {
 		clock = sinon.useFakeTimers();
 
@@ -7,48 +11,68 @@
 		div.style.width = '200px';
 		div.style.height = '200px';
 		document.body.appendChild(div);
-
-		map = L.map(div, { maxZoom: 18 });
-
+	
+		map = L.map(div, { maxZoom: 18, trackResize: false });
+	
+		// Corresponds to zoom level 8 for the above div dimensions.
 		map.fitBounds(new L.LatLngBounds([
 			[1, 1],
 			[2, 2]
 		]));
 	});
+
 	afterEach(function () {
+		if (group instanceof L.MarkerClusterGroup) {
+			group.clearLayers();
+			map.removeLayer(group);
+		}
+
+		map.remove();
+		div.remove()
 		clock.restore();
-		document.body.removeChild(div);
+
+		clock = div = map = group = null;
 	});
 
+	/////////////////////////////
+	// TESTS
+	/////////////////////////////
 	it('appears when added to the group before the group is added to the map', function () {
 
-		var group = new L.MarkerClusterGroup();
+		group = new L.MarkerClusterGroup();
+
 		var marker = new L.Circle([1.5, 1.5], 200);
 
 		group.addLayer(marker);
 		map.addLayer(group);
 
-		expect(marker._container).to.not.be(undefined);
-		expect(marker._container.parentNode).to.be(map._pathRoot);
+		// Leaflet 1.0.0 now uses an intermediate L.Renderer.
+		// marker > _path > _rootGroup (g) > _container (svg) > pane (div)
+		expect(marker._path.parentNode).to.not.be(undefined);
+		expect(marker._path.parentNode.parentNode.parentNode).to.be(map.getPane('overlayPane'));
 
 		clock.tick(1000);
 	});
+
 	it('appears when added to the group after the group is added to the map', function () {
 
-		var group = new L.MarkerClusterGroup();
+		group = new L.MarkerClusterGroup();
+
 		var marker = new L.Circle([1.5, 1.5], 200);
 
 		group.addLayer(marker);
 		map.addLayer(group);
 
-		expect(marker._container).to.not.be(undefined);
-		expect(marker._container.parentNode).to.be(map._pathRoot);
+		expect(marker._path.parentNode).to.not.be(undefined);
+		expect(marker._path.parentNode.parentNode.parentNode).to.be(map.getPane('overlayPane'));
 
 		clock.tick(1000);
 	});
+
 	it('appears animated when added to the group after the group is added to the map', function () {
 
-		var group = new L.MarkerClusterGroup({ animateAddingMarkers: true });
+		group = new L.MarkerClusterGroup({ animateAddingMarkers: true });
+
 		var marker = new L.Circle([1.5, 1.5], 200);
 		var marker2 = new L.Circle([1.5, 1.5], 200);
 
@@ -56,32 +80,34 @@
 		group.addLayer(marker);
 		group.addLayer(marker2);
 
-		expect(marker._container.parentNode).to.be(map._pathRoot);
-		expect(marker2._container.parentNode).to.be(map._pathRoot);
+		expect(marker._path.parentNode.parentNode.parentNode).to.be(map.getPane('overlayPane'));
+		expect(marker2._path.parentNode.parentNode.parentNode).to.be(map.getPane('overlayPane'));
 
 		clock.tick(1000);
 	});
 
-
 	it('creates a cluster when 2 overlapping markers are added before the group is added to the map', function () {
 
-		var group = new L.MarkerClusterGroup();
+		group = new L.MarkerClusterGroup();
+
 		var marker = new L.Circle([1.5, 1.5], 200);
 		var marker2 = new L.Circle([1.5, 1.5], 200);
 
 		group.addLayers([marker, marker2]);
 		map.addLayer(group);
 
-		expect(marker._container).to.be(undefined);
-		expect(marker2._container).to.be(undefined);
+		expect(marker._path).to.be(undefined);
+		expect(marker2._path).to.be(undefined);
 
 		expect(map._panes.markerPane.childNodes.length).to.be(1);
 
 		clock.tick(1000);
 	});
+
 	it('creates a cluster when 2 overlapping markers are added after the group is added to the map', function () {
 
-		var group = new L.MarkerClusterGroup();
+		group = new L.MarkerClusterGroup();
+
 		var marker = new L.Circle([1.5, 1.5], 200);
 		var marker2 = new L.Circle([1.5, 1.5], 200);
 
@@ -89,8 +115,8 @@
 		group.addLayer(marker);
 		group.addLayer(marker2);
 
-		expect(marker._container.parentNode).to.be(null); //Removed then re-added, so null
-		expect(marker2._container).to.be(undefined);
+		expect(marker._path.parentNode).to.be(null); //Removed then re-added, so null
+		expect(marker2._path).to.be(undefined);
 
 		expect(map._panes.markerPane.childNodes.length).to.be(1);
 
@@ -99,20 +125,20 @@
 
 	it('disappears when removed from the group', function () {
 
-		var group = new L.MarkerClusterGroup();
+		group = new L.MarkerClusterGroup();
+
 		var marker = new L.Circle([1.5, 1.5], 200);
 
 		group.addLayer(marker);
 		map.addLayer(group);
 
-		expect(marker._container).to.not.be(undefined);
-		expect(marker._container.parentNode).to.be(map._pathRoot);
+		expect(marker._path.parentNode).to.not.be(undefined);
+		expect(marker._path.parentNode.parentNode.parentNode).to.be(map.getPane('overlayPane'));
 
 		group.removeLayer(marker);
 
-		expect(marker._container.parentNode).to.be(null);
+		expect(marker._path.parentNode).to.be(null);
 
 		clock.tick(1000);
 	});
-
 });
--- libraries/leaflet_markercluster/spec/suites/RemoveLayerSpec.js
+++ libraries/leaflet_markercluster/spec/suites/RemoveLayerSpec.js
@@ -1,27 +1,46 @@
 ﻿describe('removeLayer', function () {
-	var map, div, clock;
+	/////////////////////////////
+	// SETUP FOR EACH TEST
+	/////////////////////////////
+	var clock, div, map, group;
+
 	beforeEach(function () {
 		clock = sinon.useFakeTimers();
+
 		div = document.createElement('div');
 		div.style.width = '200px';
 		div.style.height = '200px';
 		document.body.appendChild(div);
-
-		map = L.map(div, { maxZoom: 18 });
-
+	
+		map = L.map(div, { maxZoom: 18, trackResize: false });
+	
+		// Corresponds to zoom level 8 for the above div dimensions.
 		map.fitBounds(new L.LatLngBounds([
 			[1, 1],
 			[2, 2]
 		]));
 	});
+
 	afterEach(function () {
+		if (group instanceof L.MarkerClusterGroup) {
+			group.clearLayers();
+			map.removeLayer(group);
+		}
+
+		map.remove();
+		div.remove()
 		clock.restore();
-		document.body.removeChild(div);
+
+		clock = div = map = group = null;
 	});
 
+	/////////////////////////////
+	// TESTS
+	/////////////////////////////
 	it('removes a layer that was added to it', function () {
 
-		var group = new L.MarkerClusterGroup();
+		group = new L.MarkerClusterGroup();
+
 		var marker = new L.Marker([1.5, 1.5]);
 
 		map.addLayer(group);
@@ -37,7 +56,8 @@
 
 	it('doesnt remove a layer not added to it', function () {
 
-		var group = new L.MarkerClusterGroup();
+		group = new L.MarkerClusterGroup();
+
 		var marker = new L.Marker([1.5, 1.5]);
 
 		map.addLayer(group);
@@ -53,7 +73,8 @@
 
 	it('removes a layer that was added to it (before being on the map) that is shown in a cluster', function () {
 
-		var group = new L.MarkerClusterGroup();
+		group = new L.MarkerClusterGroup();
+
 		var marker = new L.Marker([1.5, 1.5]);
 		var marker2 = new L.Marker([1.5, 1.5]);
 
@@ -68,7 +89,8 @@
 
 	it('removes a layer that was added to it (after being on the map) that is shown in a cluster', function () {
 
-		var group = new L.MarkerClusterGroup();
+		group = new L.MarkerClusterGroup();
+
 		var marker = new L.Marker([1.5, 1.5]);
 		var marker2 = new L.Marker([1.5, 1.5]);
 
@@ -84,7 +106,8 @@
 
 	it('removes a layer that was added to it (before being on the map) that is individually', function () {
 
-		var group = new L.MarkerClusterGroup();
+		group = new L.MarkerClusterGroup();
+
 		var marker = new L.Marker([1, 1.5]);
 		var marker2 = new L.Marker([3, 1.5]);
 
@@ -103,7 +126,8 @@
 
 	it('removes a layer (with animation) that was added to it (after being on the map) that is shown in a cluster', function () {
 
-		var group = new L.MarkerClusterGroup({ animateAddingMarkers: true });
+		group = new L.MarkerClusterGroup({ animateAddingMarkers: true });
+
 		var marker = new L.Marker([1.5, 1.5]);
 		var marker2 = new L.Marker([1.5, 1.5]);
 
@@ -128,7 +152,8 @@
 
 	it('removes the layers that are in the given LayerGroup', function () {
 
-		var group = new L.MarkerClusterGroup();
+		group = new L.MarkerClusterGroup();
+
 		var marker = new L.Marker([1.5, 1.5]);
 		var marker2 = new L.Marker([1.5, 1.5]);
 
@@ -145,7 +170,8 @@
 
 	it('removes the layers that are in the given LayerGroup when not on the map', function () {
 
-		var group = new L.MarkerClusterGroup();
+		group = new L.MarkerClusterGroup();
+
 		var marker = new L.Marker([1.5, 1.5]);
 		var marker2 = new L.Marker([1.5, 1.5]);
 
@@ -156,6 +182,23 @@
 		group.removeLayer(layer);
 
 		expect(group.hasLayer(marker)).to.be(true);
+		expect(group.hasLayer(marker2)).to.be(false);
+	});
+
+	it('passes control to removeLayers when marker is a Layer Group', function () {
+
+		group = new L.MarkerClusterGroup();
+
+		var marker1 = new L.Marker([1.5, 1.5]);
+		var marker2 = new L.Marker([1.5, 1.5]);
+
+		group.addLayers([marker1, marker2]);
+
+		var layer = L.layerGroup();
+		layer.addLayer(marker2);
+		group.removeLayer(new L.LayerGroup([layer]));
+
+		expect(group.hasLayer(marker1)).to.be(true);
 		expect(group.hasLayer(marker2)).to.be(false);
 	});
 });
