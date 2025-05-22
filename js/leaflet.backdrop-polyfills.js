d2d9debbf68d2d90575cf9850b1239c7206c28f8
indigoxela <user@olga.indigofloat.at>
2025-05-22 13:36:26 +0200

    Cleanup polyfill js file

--- js/leaflet.backdrop-polyfills.js
+++ js/leaflet.backdrop-polyfills.js
@@ -1,17 +1,20 @@
 /**
  * @file
+ * Leaflet-V1-polyfill adds a bunch of global helper functions.
+ * We don't need all for 3rd party plugins, but many.
  */
 (function () {
   'use strict';
 
-  // Leaflet-V1-polyfill adds a bunch of global helper functions,
-  // let's see what we need - apparently most of them, but not all.
-  // @todo verify.
-  applyDomUtilPolyfill();
-  applyUtilPolyfill();
-  applyMouseEventPolyfill();
-  applyDomEventPolyfill();
-  applyDeprecatedMethodsPolyfill();
-  applyFactoryMethodsPolyfill();
+  // Needed by all plugins:
+  window.applyDomUtilPolyfill();
+  // Needed by fullscreen, markercluster:
+  window.applyUtilPolyfill();
+  // Needed by markercluster (spiderfy), zoomslider:
+  window.applyMouseEventPolyfill();
+  // Needed by markercluster:
+  window.applyDeprecatedMethodsPolyfill();
+  // Needed by fullscreen, zoomslider, viewcenter:
+  window.applyFactoryMethodsPolyfill();
 
 })();
