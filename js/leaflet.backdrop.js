c72974ba330bd96308a2fb91fa813031814177be
indigoxela <xela@indigofloat.at>
2026-02-06 13:12:52 +0100

    Issue #80: Make viewCenter fully optional (code cleanup) (#82)
    * Fix empty map has wrong zoom

--- js/leaflet.backdrop.js
+++ js/leaflet.backdrop.js
@@ -212,19 +212,6 @@
         }
 
         let zoom = this.map.settings.zoom ? this.map.settings.zoom : this.map.settings.zoomDefault;
-        // Init ViewCenter plugin with some defaults.
-        let viewCenter = new L.Control.ViewCenter({
-          position: 'topleft',
-          title: Backdrop.t('Back to the starting point'),
-          forceSeparateButton: true,
-          vcLatLng: [0, 0],
-          vcZoom: zoom
-        });
-        // @todo viewCenter is in use further down. Might need restructuring.
-        if (controls.ControlViewCenter) {
-          lMap.viewCenterControl = viewCenter;
-          lMap.addControl(viewCenter);
-        }
 
         // center the map
         if (this.map.center && (this.map.center.force || this.features.length === 0)) {
@@ -239,7 +226,7 @@
         else if (this.map.center === undefined) {
           // No points, for instance an empty views result and no default
           // center set. This prevents js errors in the library.
-          lMap.setView(new L.LatLng(0, 0), this.map.settings.minZoom);
+          lMap.setView(new L.LatLng(0, 0), zoom);
 
           // Center to current position, if module geoip_tokens is available.
           // We get the values via ajax, so there might be a slight delay.
@@ -247,7 +234,9 @@
             if (typeof Backdrop.geoipTokens.getData === 'function') {
               Backdrop.geoipTokens.getData('latlon').done(function (data) {
                 lMap.setView(new L.LatLng(data.latitude, data.longitude), zoom);
-                viewCenter.options.vcLatLng = [data.latitude, data.longitude];
+                if (typeof lMap.viewCenterControl === 'object') {
+                  lMap.viewCenterControl.options.vcLatLng = [data.latitude, data.longitude];
+                }
               });
             }
           }
@@ -258,9 +247,23 @@
         lMap.center = lMap.getCenter();
         lMap.zoom = lMap.getZoom();
 
-        // Update viewCenter options.
-        viewCenter.options.vcLatLng = [lMap.center.lat, lMap.center.lng];
-        viewCenter.options.vcZoom = lMap.zoom;
+        // Init viewCenter plugin, if enabled.
+        if (controls.ControlViewCenter) {
+          // Depending on zoom settings, we need either that, or the
+          // automatically determined value.
+          let zoomInitial = this.map.settings.zoom ? zoom : lMap.zoom;
+          let viewCenter = new L.Control.ViewCenter({
+            position: 'topleft',
+            title: Backdrop.t('Back to the starting point'),
+            forceSeparateButton: true,
+            vcLatLng: lMap.center,
+            vcZoom: zoomInitial
+          });
+
+          lMap.addControl(viewCenter);
+          // Expose for later adaption by geoipTokens.
+          lMap.viewCenterControl = viewCenter;
+        }
 
         // add attribution
         if (this.map.settings.attributionControl && this.map.attribution) {
